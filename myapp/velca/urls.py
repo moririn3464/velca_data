f14e5c21fd639146c593c339298222696d954fee
moririn3464 <moririn3464@gmail.com>
2021-12-22 15:54:59 +0900

    チーム詳細ページ作成

--- myapp/velca/urls.py
+++ myapp/velca/urls.py
@@ -5,7 +5,7 @@ app_name = 'velca'
 
 urlpatterns = [
   path('', views.index, name='index'),
-  path('player_list/', views.PlayerListView.as_view(), name='player_list'),
   path('game_data/', views.GameDataView.as_view(), name='game_data'),
   path('game_detail/<int:game_id>/', views.game_detail, name='game_detail'),
+  path('club_detail/<int:pk>/', views.ClubDetail.as_view(), name='club_detail'),
 ]
--- myapp/velca/views.py
+++ myapp/velca/views.py
@@ -1,42 +1,41 @@
 from typing import List
+from django.db import models
+from django.db.models import query
 from django.http import request
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 from django.shortcuts import render, get_object_or_404
 from .models import Player_personal, Club, Stats, Game
 
 def index(request):
   return render(request, 'velca/index.html')
 
-
-class PlayerListView(ListView):
-  model = Player_personal
-
-
 class GameDataView(ListView):
   template_name = 'velca/game_data.html'
   model = Game
-
-  # def get_context_data(self, **kwargs):
-  #   context = super(GameDataView, self).get_context_data(**kwargs)
-  #   context.update({
-  #     # 試合情報用フィルター
-  #     'velca_stats': Game.objects.filter(home_roster__players_team__club_name='長崎ヴェルカ'),
-  #     'stats_list': Stats.objects.all(),
-  #   })  
-  #   return context
-
 
 def game_detail(request, game_id):
   game_detail = Game.objects.get(id=game_id)
   game_stats = game_detail.game_stats.all()
   velca_stats = game_detail.game_stats.filter(player__players_team=1)
   opponent_stats = game_detail.game_stats.filter(player__players_team=2)
-  # def get_context_data(self, **kwargs):
-  #   context = super(game_detail, self).get_context_data(**kwargs)
-  #   context.update({
-  #       # 試合情報用フィルター
-  #       'velca_stats': game_detail.game_stats.all,
-  #   })
-  #   return context
 
-  return render(request, 'velca/game_detail.html', {'game_detail': game_detail, 'velca_stats': velca_stats, 'game_stats': game_stats, 'opponent_stats': opponent_stats})
+  return render(
+    request, 
+    'velca/game_detail.html', 
+    {
+      'game_detail': game_detail, 
+      'velca_stats': velca_stats, 
+      'game_stats': game_stats, 
+      'opponent_stats': opponent_stats
+    })
+
+
+class ClubDetail(DetailView):
+  model = Club
+  template_name = "velca/club_detail.html"
+
+  def get_context_data(self, **kwargs):
+      context = super().get_context_data(**kwargs)
+      club_player_list = Player_personal.objects.filter(players_team_id=self.kwargs['pk'])  # pkを指定してデータを絞り込む
+      context['club_player_list'] = club_player_list
+      return context
