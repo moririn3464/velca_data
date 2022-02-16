e22c4355450af744b042466f054c16608001c050
moririn3464 <moririn3464@gmail.com>
2022-02-16 15:44:59 +0900

    生年月日フィールド追加

--- myapp/velca/models/player_personal.py
+++ myapp/velca/models/player_personal.py
@@ -2,6 +2,7 @@ from typing import Callable
 from django.db import models
 from django.db.models.deletion import CASCADE
 from django.db.models.fields.related import OneToOneField
+from jp_birthday.models import BirthdayModel
 from .playername import Playername
 from .club import Club
 
@@ -9,6 +10,7 @@ class Player_personal(models.Model):
   name = models.OneToOneField(Playername,on_delete=CASCADE, related_name="player_name")
   jersey_number = models.IntegerField(default=0,verbose_name='背番号')
   players_team = models.ForeignKey(Club, on_delete=models.CASCADE, related_name="players_team")
+  player_birthday = models.DateTimeField(blank=True, null=True)
 
 
   class Meta:
